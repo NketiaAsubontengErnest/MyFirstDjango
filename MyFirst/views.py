076ee3a92eef946ae9982df774696165cf4ed200
Ernest <nketiaernest18@gmail.com>
2021-04-14 01:48:11 -0700

    my Assignment

---
+++ MyFirst/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello I am Nketia Aubonteng Ernest I am from ghana and I want to be a very good programer")
